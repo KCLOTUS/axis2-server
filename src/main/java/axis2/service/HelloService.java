fad5795107d52b0ed05c56d4e8dee0f793821c64
15179156337 <2350602201@qq.com>
2017-12-05 11:18:15 +0800

    yes

---
+++ src/main/java/axis2/service/HelloService.java
@@ -0,0 +1,7 @@
+package axis2.service;
+
+public interface HelloService {
+
+    public String sayHello(String content);
+
+}
---
+++ src/main/java/axis2/service/HelloServiceImpl.java
@@ -0,0 +1,9 @@
+package axis2.service;
+
+public class HelloServiceImpl implements HelloService {
+
+    public String sayHello(String content) {
+        return "hello," + content;
+    }
+
+}
